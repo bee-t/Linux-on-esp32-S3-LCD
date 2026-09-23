11b6416ddbecd216681ff0270dccf75fc31dd742
Abdul Basit Tahir <abtcr7@gmail.com>
2026-09-23 23:51:17 +0200

    Auto-launch epd-shell from shell profile to fix setsid() failure

---
+++ new-files/board/espressif/esp32s3/rootfs_overlay/etc/profile.d/epd-shell.sh
@@ -0,0 +1,5 @@
+# If we are on the serial console and not already in epd-shell, launch it
+if [ -c /dev/epd ] && [ -n "$PS1" ] && [ -z "$EPD_SHELL_ACTIVE" ]; then
+    export EPD_SHELL_ACTIVE=1
+    exec epd-shell
+fi
